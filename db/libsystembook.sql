b99042593a2b3334d2d987a89b3c209995b45cd8
bichninh <ninhhabk@gmail.com>
2020-11-16 23:58:18 +0700

    co so du lieu

---
+++ db/libsystembook.sql
@@ -0,0 +1,250 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 16, 2020 lúc 04:38 PM
+-- Phiên bản máy phục vụ: 10.4.8-MariaDB
+-- Phiên bản PHP: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `libsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(60) CHARACTER SET latin1 NOT NULL,
+  `firstname` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `lastname` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `photo` varchar(200) CHARACTER SET latin1 NOT NULL,
+  `created_on` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`, `photo`, `created_on`) VALUES
+(1, 'bichbich', '123456', 'do', 'huong', '', '2020-10-20');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `isbn` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `title` text CHARACTER SET latin1 NOT NULL,
+  `author` varchar(150) CHARACTER SET latin1 NOT NULL,
+  `quality` int(30) NOT NULL,
+  `publisher` varchar(150) CHARACTER SET latin1 NOT NULL,
+  `publish_date` date NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `books`
+--
+
+INSERT INTO `books` (`id`, `isbn`, `category_id`, `title`, `author`, `quality`, `publisher`, `publish_date`, `status`) VALUES
+(1, 'testisbn12345', 1, 'Sample Title', 'Sample Author', 20, 'My Printers', '2018-05-21', 1),
+(2, 'bookisbntest', 2, 'This is a Sample Title of a Sample Book', 'Author Me', 5, 'Self Publish Inc', '2018-05-02', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `borrow`
+--
+
+CREATE TABLE `borrow` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `date_borrow` date NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `borrow`
+--
+
+INSERT INTO `borrow` (`id`, `student_id`, `book_id`, `date_borrow`, `status`) VALUES
+(15, 3, 1, '2020-10-04', 1),
+(16, 3, 2, '2020-10-04', 1),
+(17, 3, 1, '2020-10-04', 0),
+(18, 3, 2, '2020-10-04', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Engineering'),
+(2, 'Mathematics'),
+(3, 'Science and Technology'),
+(4, 'History');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `returns`
+--
+
+CREATE TABLE `returns` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `date_return` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `returns`
+--
+
+INSERT INTO `returns` (`id`, `student_id`, `book_id`, `date_return`) VALUES
+(1, 3, 2, '2020-10-20'),
+(2, 3, 1, '2020-10-20'),
+(3, 3, 2, '2020-10-20'),
+(4, 3, 2, '2020-10-20');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `student_username` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `student_pass` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `firstname` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `lastname` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `emaill` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `photo` varchar(200) CHARACTER SET latin1 NOT NULL,
+  `created_on` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `students`
+--
+
+INSERT INTO `students` (`id`, `student_username`, `student_pass`, `firstname`, `lastname`, `emaill`, `photo`, `created_on`) VALUES
+(3, 'ha', '123', 'on', 'ha', 'hanhu@gmail.com', '', '2020-10-20'),
+(4, 'bich', '1234', 'ninh', 'bich', 'ninhbich@gmail.com', '', '2020-10-20');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `borrow`
+--
+ALTER TABLE `borrow`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `returns`
+--
+ALTER TABLE `returns`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT cho bảng `borrow`
+--
+ALTER TABLE `borrow`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT cho bảng `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `returns`
+--
+ALTER TABLE `returns`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
