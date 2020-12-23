17f27e0de45097be1ae33dcc63ea8cb23e3c29be
bichninh <ninhhabk@gmail.com>
2020-12-23 19:59:01 +0700

    student xem cac book minh da muon

---
+++ students/viewborrowed.php
@@ -0,0 +1,112 @@
+<?php include 'include2/session.php'; ?>
+<?php include 'include2/header.php'; ?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <?php include 'include2/navbar.php'; ?>
+  <?php include 'include2/menubar.php'; ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Borrow Books
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li>Transaction</li>
+        <li class="active">Borrow</li>
+      </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+      <?php
+        if(isset($_SESSION['error'])){
+          ?>
+            <div class="alert alert-danger alert-dismissible">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+              <h4><i class="icon fa fa-warning"></i> Error!</h4>
+                <ul>
+                <?php
+                  foreach($_SESSION['error'] as $error){
+                    echo "
+                      <li>".$error."</li>
+                    ";
+                  }
+                ?>
+                </ul>
+            </div>
+          <?php
+          unset($_SESSION['error']);
+        }
+
+        if(isset($_SESSION['success'])){
+          echo "
+            <div class='alert alert-success alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+              <h4><i class='icon fa fa-check'></i> Success!</h4>
+              ".$_SESSION['success']."
+            </div>
+          ";
+          unset($_SESSION['success']);
+        }
+      ?>
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header with-border">
+              
+            </div>
+            <div class="box-body">
+              <table id="example1" class="table table-bordered">
+                <thead>
+                  <th class="hidden"></th>
+                  <th>Book ID</th>
+                  <th>Student ID</th>
+                  
+                  
+                  <th>Date Borrow</th>
+                  <th> Status</th>
+                  
+                </thead>
+                <tbody>
+                  <?php
+                    $sql = "SELECT *FROM borrow LEFT JOIN students ON students.id=borrow.student_id LEFT JOIN books ON books.id=borrow.book_id WHERE students.id='".$_SESSION['students']."' ORDER BY date_borrow DESC ";
+                    $query = $conn->query($sql);
+                    while($row = $query->fetch_assoc()){
+                      if($row['status'] == 1){
+                        $status = '<span class="label label-danger">notreturn</span>';
+                      }
+                      else{
+                        $status = '<span class="label label-success">returned</span>';
+                      }
+                      
+                      echo "
+                        <tr>
+                          <td class='hidden'></td>
+                          <td>".$row['book_id']."</td>
+                          <td>".$row['student_id']."</td>
+                          <td>".date('M d, Y', strtotime($row['date_borrow']))."</td>
+                          <td>".$status."</td>
+                         
+                        </tr>
+                      ";
+                    }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>   
+  </div>
+    
+  <?php include 'include2/footer.php'; ?>
+  <?php include 'include2/borrow_modal.php'; ?>
+</div>
+<?php include 'include2/scripts.php'; ?>
+
+</body>
+</html>
