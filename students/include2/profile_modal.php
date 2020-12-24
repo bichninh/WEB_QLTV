f6bc9cedcc81696aa792ba408d565491c7a7a352
Huong-hihi <dothihuong030499@gmail.com>
2020-12-24 08:50:49 +0700

    include2

---
+++ students/include2/profile_modal.php
@@ -0,0 +1,70 @@
+<!-- Add -->
+<div class="modal fade" id="profile">
+    <div class="modal-dialog">
+        <div class="modal-content">
+          	<div class="modal-header">
+            	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              		<span aria-hidden="true">&times;</span></button>
+            	<h4 class="modal-title"><b> Student Profile</b></h4>
+          	</div>
+          	<div class="modal-body">
+            	<form class="form-horizontal" method="POST" action="profile_update.php?return=<?php echo basename($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data">
+          		  <div class="form-group">
+                  	<label for="student_username" class="col-sm-3 control-label">Username</label>
+
+                  	<div class="col-sm-9">
+                    	<input type="text" class="form-control" id="student_username" name="student_username" value="<?php echo $user['student_username']; ?>">
+                  	</div>
+                </div>
+                <div class="form-group">
+                    <label for="student_pass" class="col-sm-3 control-label">Password</label>
+
+                    <div class="col-sm-9"> 
+                      <input type="student_pass" class="form-control" id="student_pass" name="student_pass" value="<?php echo $user['student_pass']; ?>">
+                    </div>
+                </div>
+                <div class="form-group">
+                  	<label for="firstname" class="col-sm-3 control-label">Firstname</label>
+
+                  	<div class="col-sm-9">
+                    	<input type="text" class="form-control" id="firstname" name="firstname" value="<?php echo $user['firstname']; ?>">
+                  	</div>
+                </div>
+                <div class="form-group">
+                  	<label for="lastname" class="col-sm-3 control-label">Lastname</label>
+
+                  	<div class="col-sm-9">
+                    	<input type="text" class="form-control" id="lastname" name="lastname" value="<?php echo $user['lastname']; ?>">
+                  	</div>
+                </div>
+                <div class="form-group">
+                  	<label for="lastname" class="col-sm-3 control-label"> Email</label>
+
+                  	<div class="col-sm-9">
+                    	<input type="text" class="form-control" id="emaill" name="emaill" value="<?php echo $user['emaill']; ?>">
+                  	</div>
+                </div>
+                <div class="form-group">
+                    <label for="photo" class="col-sm-3 control-label">Photo:</label>
+
+                    <div class="col-sm-9">
+                      <input type="file" id="photo" name="photo">
+                    </div>
+                </div>
+                <hr>
+                <div class="form-group">
+                    <label for="curr_password" class="col-sm-3 control-label">Current Password:</label>
+
+                    <div class="col-sm-9">
+                      <input type="password" class="form-control" id="curr_password" name="curr_password" placeholder="input current password to save changes" required>
+                    </div>
+                </div>
+          	</div>
+          	<div class="modal-footer">
+            	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+            	<button type="submit" class="btn btn-success btn-flat" name="save"><i class="fa fa-check-square-o"></i> Save</button>
+            	</form>
+          	</div>
+        </div>
+    </div>
+</div>
